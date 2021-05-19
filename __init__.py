3f352425b3bf371365255ed554ff1dbc31e0a7cd
zhiao777774 <zhiaohsu@gmail.com>
2021-05-19 13:56:23 +0800

    優化影像辨識程式碼

--- __init__.py
+++ __init__.py
@@ -1,58 +1,71 @@
-import os
+import sys
+import signal
 import time
 import cv2
-import matplotlib.pyplot as plt
-from pathlib import Path
-from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+from tensorflow.keras.models import load_model
 
 
 def initialize():
-    cap = cv2.VideoCapture(0)
-    time.sleep(1)
-    
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 630)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+    if len(sys.argv) < 2:
+        print('Arguments: <mode>')
+        sys.exit(1)
+    mode = sys.argv[1]
 
-    print(cap.isOpened())
-    if not cap.isOpened():
-        cap.open(0)
+    _signal_handle()
+    if mode == 'camera':
+        capture = cv2.VideoCapture(0)
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH, 630)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
-    time.sleep(0.1)
+        if not capture.isOpened():
+            capture.open(0)
 
-    while True:
-        frame = cap.read()[1]
-        cv2.namedWindow('result', cv2.WINDOW_NORMAL)
-        cv2.imshow('result', frame)
+        time.sleep(.1)
+        classifier = load_model('./model/')
+        predict_result = None
+        while True:
+            frame = capture.read()[1]
+            cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+            cv2.imshow('frame', frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('s'):
-            # cv2.imwrite('/datasets/result.png', frame)
-            
-            cap.release()
-            cv2.destroyAllWindows()
-            break
-        elif cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            if cv2.waitKey(1) & 0xFF == ord('s'):
+                cv2.imwrite(
+                    f'./assets/result/{time.strftime("%Y%m%d-%H%M", time.localtime())}.png', frame)
 
-        
+                classes = {0: 'Healthy', 1: 'Parkinsons\'s'}
+                pred = classifier.predict(frame)
+                clas = classes[np.round(pred)]
+                print(f'預測機率: {pred}')
+                print(f'分類結果: {clas}')
 
-def plot():
-    plt.style.use('dark_background')
-    _DATA_PATH_STR = 'D:\\桌面\\Python\\parkinsons\\datasets\\drawings'
-    _DATA_PATH_OBJ = Path(_DATA_PATH_STR)
+                predict_result = {
+                    'frame': frame,
+                    'prob': pred,
+                    'clas': clas
+                }
 
-    plt.figure(figsize=(12, 12))
-    for i in range(1, 10, 1):
-        plt.subplot(3, 3, i)
-        img = load_img(f"{_DATA_PATH_STR}/spiral/training/healthy/" +
-                       os.listdir(f"{_DATA_PATH_STR}/spiral/training/healthy")[i])
-        plt.imshow(img)
+                capture.release()
+                cv2.destroyAllWindows()
+                break
+
+        cv2.text(predict_result.frame, f'分類結果: {predict_result.clas}',
+                 (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (25, 25, 112), 1, cv2.LINE_AA)
+        cv2.imshow('predict', predict_result.frame)
+        while True:
+            if not cv2.waitKey(1) is -1:
+                cv2.destroyAllWindows()
+                break
+    elif mode == 'speech':
+        print('speech')
+    else:
+        print('Arguments: <mode> must be "camera" or "speech".')
 
 
-    plt.figure(figsize=(12, 12))
-    for i in range(1, 10):
-        plt.subplot(3, 3, i)
-        img = load_img(f"{_DATA_PATH_STR}/spiral/training/parkinson/" +
-                       os.listdir(f"{_DATA_PATH_STR}/spiral/training/parkinson")[i])
-        plt.imshow(img)
+def _signal_handle():
+    def _handler(signal, frame):
+        cv2.destroyAllWindows()
+        sys.exit(0)
 
-    plt.show()
+    signal.signal(signal.SIGINT, _handler)
+    signal.signal(signal.SIGTERM, _handler)
